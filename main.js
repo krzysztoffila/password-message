9c94f5ce3b21678dcf32ce687a66b8352b8c4201
krzysztoffila <umowy.fila@gmail.com>
2023-03-06 16:41:43 +0100

    v3

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 const input = document.querySelector('input');
 const password = 'user';
 const message = "Nie ma mnie w domu";
+const div = document.querySelector('div')
 
 input.addEventListener('input', e => {
     if (e.target.value === password) {
-        const div = document.querySelector('div')
         div.textContent = message
+        e.target.value = ''
+    } else {
+        div.textContent = ""
     }
 })
